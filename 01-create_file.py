3106b211cfe707deee3c730a3147575487b788f8
noribang <hoonk9387@gmail.com>
2018-07-10 12:32:54 -0500

    Added script to create, write to, close and read .txt file.

---
+++ 01-create_file.py
@@ -0,0 +1,20 @@
+# Reference: http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
+#            http://www.pythonforbeginners.com/cheatsheet/python-file-handling
+
+# Create .txt file and make it writeable.
+file = open("testfile.txt", "w")
+
+# Write lines of text to file.
+file.write("Hello World.")
+file.write("This is our new text file")
+file.write("and this is another line.")
+file.write("Why? Because we can.")
+
+# Close file.
+file.close()
+
+# 
+readFile = open("testfile.txt", "r")
+
+# Read file and print.
+print readFile.read()
